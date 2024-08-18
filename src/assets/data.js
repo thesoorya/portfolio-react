af942518b58247535a88cc3031fe7ae8a28a692c
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-08-18 20:29:04 +0530

    assets added

---
+++ src/assets/data.js
@@ -0,0 +1,93 @@
+import jwt from './jwt.png';
+import cafe from './cafe.png';
+import ecom from './ecom.png';
+import bookstore from './bookstore.png';
+import movie from './movie.png';
+import weather from './weather.png';
+import todo from './todo.png';
+import podia from './podia.png';
+import rps from './rps.png'; // Correct image for rps
+import cartfunc from './cartfunc.png'; // Correct image for cartfunc
+
+export const projects = [
+  {
+    id: 1,
+    title: "Project 1",
+    link: "https://example.com/project1",
+    description: "This is the first project.",
+    image: jwt,
+    github: false
+  },
+  {
+    id: 2,
+    title: "Project 2",
+    link: "https://example.com/project2",
+    description: "This is the second project.",
+    image: cafe,
+    github: false
+  },
+  {
+    id: 3,
+    title: "Project 3",
+    link: "https://example.com/project3",
+    description: "This is the third project.",
+    image: ecom,
+    github: false
+  },
+  {
+    id: 4,
+    title: "Project 4",
+    link: "https://example.com/project4",
+    description: "This is the fourth project.",
+    image: bookstore,
+    github: false
+  },
+  {
+    id: 5,
+    title: "Project 5",
+    link: "https://example.com/project5",
+    description: "This is the fifth project.",
+    image: movie,
+    github: false
+  },
+  {
+    id: 6,
+    title: "Project 6",
+    link: "https://example.com/project6",
+    description: "This is the sixth project.",
+    image: weather,
+    github: false
+  },
+  {
+    id: 7,
+    title: "Project 7",
+    link: "https://example.com/project7",
+    description: "This is the seventh project.",
+    image: todo,
+    github: false
+  },
+  {
+    id: 8,
+    title: "Project 8",
+    link: "https://example.com/project8",
+    description: "This is the eighth project.",
+    image: rps, // Corrected image
+    github: false
+  },
+  {
+    id: 9,
+    title: "Project 9",
+    link: "https://example.com/project9",
+    description: "This is the ninth project.",
+    image: podia,
+    github: false
+  },
+  {
+    id: 10,
+    title: "Project 10",
+    link: "https://example.com/project10",
+    description: "This is the tenth project.",
+    image: cartfunc, // Corrected image
+    github: false
+  },
+];
