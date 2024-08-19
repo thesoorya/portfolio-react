f5be1d82f9c2fb481909ac7415ceac09cf77fce3
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-08-19 17:02:16 +0530

    footer created

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        &copy; Copyright Soorya Narayanan. All Rights Reserved
+    </div>
+  )
+}
+
+export default Footer
