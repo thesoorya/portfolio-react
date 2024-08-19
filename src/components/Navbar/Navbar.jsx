11043bbca9ab759278f9b6203c482240da462911
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-08-19 17:01:38 +0530

    navbar backgroundcolor changed to dynamic

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
     const [activeLink, setActiveLink] = useState('portfolio');
 
+    useEffect(() => {
+        const savedLink = localStorage.getItem('activeLink');
+        if (savedLink) {
+            setActiveLink(savedLink);
+        }
+    }, []);
+
     const handleLinkClick = (link) => {
         setActiveLink(link);
+        localStorage.setItem('activeLink', link); 
+    };
+
+    const getNavBarStyle = () => {
+        if (activeLink === 'portfolio') {
+            return { backgroundColor: '#fbfbfb' };
+        } else if (activeLink === 'about') {
+            return { backgroundColor: '#1d1814', color: '#fbfbfb' };
+        } else {
+            return { backgroundColor: '#191815' };
+        }
     };
 
     return (
         <div>
-            <nav className="nav">
+            <nav className="nav" style={getNavBarStyle()}>
                 <div className="brand" id="brand">
-                    <Link to='/'><p>Soorya Narayanan</p></Link>
+                    <Link to='/' onClick={() => handleLinkClick('portfolio')}><p>Soorya Narayanan</p></Link>
                 </div>
                 <ul className="navigators">
                     <li className="nav-items">
