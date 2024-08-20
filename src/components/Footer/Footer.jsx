d04fe1c85f63036ce04a51baad6bf770e10976db
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-08-20 17:06:06 +0530

    footer minor css change

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
+import { useLocation } from 'react-router-dom';
 
 const Footer = () => {
-  return (
-    <div className='footer'>
-        &copy; Copyright Soorya Narayanan. All Rights Reserved
-    </div>
-  )
-}
+  const location = useLocation();
 
-export default Footer
+  const getFooterStyle = () => {
+    if (location.pathname === '/') {
+      return { backgroundColor: '#fbfbfb', color: '#191815' };
+    } else if (location.pathname === '/about') {
+      return { backgroundColor: '#1d1814', color: '#fdd596' };
+    }
+    return {};
+  };
+
+  return (
+    <div className='footer' style={getFooterStyle()}>
+      &copy; Copyright Soorya Narayanan. All Rights Reserved
+    </div>
+  );
+};
+
+export default Footer;
