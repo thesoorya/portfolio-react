1ea43a7a40fc159254447c42248ae84f239c1818
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-08-21 20:48:48 +0530

    projects details page completed

--- src/pages/ProjectDetails/ProjectDetails.jsx
+++ src/pages/ProjectDetails/ProjectDetails.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import './ProjectDetails.css'
+import React, { useContext } from 'react';
+import './ProjectDetails.css';
+import { useParams } from 'react-router-dom';
+import { ContextProvider } from '../../components/Context/StoreContext';
 
 const ProjectDetails = () => {
-  return (
-    <div>ProjectDetails</div>
-  )
-}
+  const { projects } = useContext(ContextProvider);
+  const { title } = useParams();
 
-export default ProjectDetails
+  const formattedTitle = title.replace(/-/g, ' ');
+
+  const project = projects.find(project => project.title === formattedTitle);
+
+  if (!project) {
+    return <div>Project not found</div>;
+  }
+
+  return (
+    <div className='project'>
+      {!project.github && project.previewImage && (
+        <div className="projectImageContainer">
+          <img src={project.previewImage} alt={project.title} />
+        </div>
+      )}
+      <div className="projectDetails">
+        <div className='projectDetail'>
+          <h1>{project.title}</h1>
+          <p>{project.description}</p>
+        </div>
+        <div className='projectLinks'>
+          <div>
+            <a href={project.link}>
+              <p><u>{project.github ? 'View on GitHub' : 'Live Demo'}</u></p>
+            </a>
+          </div>
+          <p>Built By: <small>{project.builtBy}</small></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetails;
