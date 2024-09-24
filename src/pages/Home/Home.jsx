71b44428e81c1c88adc73c6d18217d916258431a
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-09-24 13:58:31 +0530

    removed framer motion

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,24 @@
-import React, { useContext } from 'react';
-import Project from '../../components/Project/Project';
-import './Home.css';
-import { ContextProvider } from '../../components/Context/StoreContext';
-import { motion } from 'framer-motion';;
+import React, { useContext } from "react";
+import Project from "../../components/Project/Project";
+import "./Home.css";
+import { ContextProvider } from "../../components/Context/StoreContext";
+import { motion } from "framer-motion";
 
 const Home = () => {
-    const { projects } = useContext(ContextProvider);
+  const { projects } = useContext(ContextProvider);
 
-    return (
-        <motion.div
-            className="home"
-            initial="hidden"
-            animate="visible"
-            exit={{ opacity: 0, transition: { duration: 0.3 } }}
-        >
-            {projects.map((project, i) => (
-                <Project image={project.image} title={project.title} id={project.id} key={i} />
-            ))}
-        </motion.div>
-    );
+  return (
+    <div className="home">
+      {projects.map((project, i) => (
+        <Project
+          image={project.image}
+          title={project.title}
+          id={project.id}
+          key={i}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Home;
