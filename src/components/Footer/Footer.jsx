3b842ce11875b8216df709fd8e224f66e128dac2
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-09-24 14:05:58 +0530

    font changed in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
     if (location.pathname === '/') {
       return { backgroundColor: '#fbfbfb', color: '#191815' };
     } else if (location.pathname === '/about') {
-      return { backgroundColor: '#1d1814', color: '#fdd596', fontFamily: 'Clash Display, sans-serif' };
+      return { backgroundColor: '#1d1814', color: '#fdd596' };
     }
     else {
       return { backgroundColor: '#fbfbfb', color: '#191815' };
